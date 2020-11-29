20ad3472d0a79ee487f1f4a3414ec02981549db9
01elenaroxana02@gmail.com <01elenaroxana02@gmail.com>
2020-11-29 22:47:24 +0200

    Add project files.

---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Rus_Roxana_Lab8.Data;
+using Rus_Roxana_Lab8.Models;
+
+namespace Rus_Roxana_Lab8.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Rus_Roxana_Lab8.Data.Rus_Roxana_Lab8Context _context;
+
+        public IndexModel(Rus_Roxana_Lab8.Data.Rus_Roxana_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Category = await _context.Category.ToListAsync();
+        }
+    }
+}
